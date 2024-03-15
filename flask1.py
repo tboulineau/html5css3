463ea8032d4d0ad89b6af646c13b9fc5bf9f2407
Thomas Boulineau <th.boulineau@gmail.com>
2024-03-15 10:49:18 +0100

    fin de séance, tout fonctionne

--- flask1.py
+++ flask1.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import json
+import random
 
 app = Flask(__name__,static_folder=".",static_url_path='',template_folder='.')
 
@@ -24,7 +25,7 @@ def quiz():
     difficulte = request.args.get('difficulte')
     theme = request.args.get('theme')
 
-  with open("questions.json","r") as fichier:
+  with open("questions.json","r", encoding='utf-8') as fichier:
     data = json.load(fichier)
 
   # Accéder à l'attribut "question"*
@@ -35,26 +36,28 @@ def quiz():
     
   questions = []
   reponses = []
+  corrections = []
   '''
   print("affichage test")
   for i in range(0, 4):
      print("question " + str(i) + " : " + data["animaux"][i]["question"])
      print("reponses " + str(i) + " : " + str(data["animaux"][i]["options"]))
   '''
-  
-  for i in range(0, 3):
-     print(i)
-     questions.append(data["animaux"][i]["question"])
-     print(questions[i])
-     reponses.append(data["animaux"][i]["options"])
-     print(reponses[i])
 
-  print("questions : " + str(questions))
-  print("réponses : " + str(reponses))
+  valeurs = random.sample(range(0, len(data["animaux"])), 3)
+
+  for i in valeurs:
+     questions.append(data["animaux"][i]["question"])
+     reponses.append(data["animaux"][i]["options"])
+     corrections.append(data["animaux"][i]["answer"])
+
+  #print("questions : " + str(questions))
+  #print("réponses : " + str(reponses))
   
   return render_template('question_template.html',
-                         questions = questions,
-                         reponses = reponses)
+                         questions = json.dumps(questions),
+                         reponses = json.dumps(reponses),
+                          correction = json.dumps(corrections))
 
 if __name__=='__main__':
 	app.run(debug=True,host='0.0.0.0')
